681f4b97cae17da8795a090ec8f9239b84ffbc2d
Adam <adam.wolanski94@gmail.com>
2018-01-17 20:07:15 +0100

    Parallax tests

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrollingBackground : MonoBehaviour {
+public class ScrollingBackground : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+    public Transform[] Backgrounds;
+    public float Smoothing = 1f;
+
+    private float[] _parallaxAmount;
+    private Transform _cameraTransform;
+    private Vector3 _previousCamPosition;
+
+    private void Awake()
+    {
+        _cameraTransform = Camera.main.transform;
+    }
+
+	private void Start()
+	{
+	    _previousCamPosition = _cameraTransform.position;
+	    _parallaxAmount = new float[Backgrounds.Length];
+
+	    for (int i = 0; i < Backgrounds.Length; ++i)
+	    {
+	        _parallaxAmount[i] = Backgrounds[i].position.z * -1;
+	    }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private void Update()
+    {
+        for (int i = 0; i < Backgrounds.Length; ++i)
+        {
+            float parallax = (_previousCamPosition.x - _cameraTransform.position.x) * _parallaxAmount[i];
+            float backgroundTargetPosX = Backgrounds[i].position.x + parallax;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, Backgrounds[i].position.y, Backgrounds[i].position.z);
+
+            Backgrounds[i].position = Vector3.Lerp(Backgrounds[i].position, backgroundTargetPos,
+                Smoothing * Time.deltaTime);
+        }
+        _previousCamPosition = _cameraTransform.position;
+    }
 }
