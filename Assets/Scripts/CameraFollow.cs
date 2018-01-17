b35868fb15d58ba316b866fe8c1e78a6b7ebced6
Adam <adam.wolanski94@gmail.com>
2018-01-17 20:32:30 +0100

    Camera follow script added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public float Smoothing = 0.5f;
+    public Vector3 Offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPos = Target.position + Offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, Smoothing);
+        transform.position = smoothedPos;
+
+        transform.LookAt(Target);
+    }
+
+}
