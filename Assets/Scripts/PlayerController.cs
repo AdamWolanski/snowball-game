89958ab245173d36aab77ee9dfe28ad4db8e398e
Adam <adam.wolanski94@gmail.com>
2018-01-25 20:07:10 +0100

    PlayerController refactoring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,89 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum PlayerState { Ground, Air, Fly };
+public enum PlayerState { GROUND, AIR, FLY };
+public enum InputType { TOUCH, KEYBOARD };
 
-public class PlayerController : MonoBehaviour {
-    public delegate void Action();
-    public static event Action OnAction;
+public class PlayerController : MonoBehaviour
+{
+    private delegate void Action();
+    private Action _playerAction;
+    private InputType _playerInputType;
+    private PlayerState _playerState;
+    public PlayerState PlayerState
+    {
+        set
+        {
+            _playerState = value;
+            ActionChange(value);
+        }
+    }
 
-    private PlayerState currentState = PlayerState.Ground;
-    private PlayerState prevState = PlayerState.Ground;
-
-    public float GroundDist;
-    public float SpriteOffset;
 
     private void Start()
     {
-        OnAction += Jump;
+        Initialize();
     }
 
     private void Update()
     {
-        // if ground check == false -> OnAction += Air else OnAction += Ground
-        // if hit bird -> OnAction -> Fly
-
-        if ( StateCheck() )
+        if (GroundCheck())
         {
-            prevState = currentState;
+
         }
-#if UNITY_EDITOR || UNITY_STANDALONE
+
+        if (InputCheck())
+        {
+            _playerAction();
+        }
+    }
+
+    private bool GroundCheck()
+    {
+        return false;
+    }
+
+    private bool InputCheck()
+    {
+#if UNITY_STANDALONE || UNITY_EDITOR
         if (Input.GetKeyDown(KeyCode.Space))
         {
 #else
-        if (TouchController.Tapped())
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
 #endif
-            OnAction();
+            return true;
         }
-        if (Input.GetKeyDown(KeyCode.C))
+
+        return false;
+    }
+
+    private void Initialize()
+    {
+        PlayerState = PlayerState.GROUND;
+#if UNITY_STANDALONE || UNITY_EDITOR
+        _playerInputType = InputType.KEYBOARD;
+#else
+        _playerInputType = InputType.TOUCH;
+#endif
+    }
+
+    private void ActionChange(PlayerState action)
+    {
+        switch (action)
         {
-            currentState++;
-        }
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.Raycast(transform.position, -Vector2.up, GroundDist + SpriteOffset);
-    }
-
-    private void StateChange(PlayerState newState)
-    {
-        OnAction -= Jump;
-        OnAction -= Shrink;
-        OnAction -= Fly;
-
-        switch (currentState)
-        {
-            case PlayerState.Ground:
-                OnAction += Jump;
+            case PlayerState.GROUND:
+                _playerAction = Jump;
                 break;
-            case PlayerState.Air:
-                OnAction += Shrink;
+            case PlayerState.AIR:
+                _playerAction = Shrink;
                 break;
-            case PlayerState.Fly:
-                OnAction += Fly;
+            case PlayerState.FLY:
+                _playerAction = Fly;
                 break;
         }
-    }
-
-    private bool StateCheck()
-    {
-        return (currentState == prevState);
     }
 
     private void Jump()
     {
-        Debug.Log("jump func");
+        Debug.Log("Jump!");
     }
 
     private void Shrink()
     {
-        Debug.Log("shrink func");
+        Debug.Log("Shrink!");
     }
 
     private void Fly()
     {
-        Debug.Log("fly func");
+        Debug.Log("Lets fly!");
     }
 }
